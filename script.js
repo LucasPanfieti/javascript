2f3bbfe517273b1492811b4527367946ee24961d
Lucas Panfieti <158314592+LucasPanfieti@users.noreply.github.com>
2025-07-17 18:28:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('Isso é um alerta, clique em "OK"')
+//faz um alerta
+
+document.getElementById('nome').value = 'Oi'
+/*
+	selecionar um elemento no DOM
+	atualizar o valor desse elemento com uma string
+*/
