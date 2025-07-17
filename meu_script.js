05f19b8a517ed1e52476835e9764ac653962e8b4
Lucas Panfieti <158314592+LucasPanfieti@users.noreply.github.com>
2025-07-17 18:28:19 -0300

    Delete meu_script.js

--- meu_script.js
+++
@@ -1,8 +0,0 @@
-alert('Isso é um alerta, clique em "OK"')
-//faz um alerta
-
-document.getElementById('nome').value = 'Oi'
-/*
-	selecionar um elemento no DOM
-	atualizar o valor desse elemento com uma string
-*/
